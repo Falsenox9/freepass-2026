e027bbbc8c54b4f573a1ce7bce9135bee576f08b
unknown <daffaatha2806@gmail.com>
2026-02-04 22:46:59 +0700

    feat: user now can order food

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,15 +3,17 @@ package repository
 import "gorm.io/gorm"
 
 type Repository struct {
-    UserRepository  IUserRepository
-    AdminRepository IAdminRepository
-    FoodRepository  IFoodRepository
+	UserRepository  IUserRepository
+	AdminRepository IAdminRepository
+	FoodRepository  IFoodRepository
+	OrderRepository IOrderRepository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
-    return &Repository{
-        UserRepository:  NewUserRepository(db),
-        AdminRepository: NewAdminRepository(db),
-        FoodRepository:  NewFoodRepository(db),
-    }
+	return &Repository{
+		UserRepository:  NewUserRepository(db),
+		AdminRepository: NewAdminRepository(db),
+		FoodRepository:  NewFoodRepository(db),
+		OrderRepository: NewOrderRepository(db),
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,21 +1,23 @@
 package service
 
 import (
-    "freepass-2026/internal/repository"
-    "freepass-2026/pkg/bcrypt"
-    "freepass-2026/pkg/jwt"
+	"freepass-2026/internal/repository"
+	"freepass-2026/pkg/bcrypt"
+	"freepass-2026/pkg/jwt"
 )
 
 type Service struct {
-    UserService  IUserService
-    AdminService IAdminService
-    FoodService  IFoodService
+	UserService  IUserService
+	AdminService IAdminService
+	FoodService  IFoodService
+	OrderService IOrderService
 }
 
 func NewService(repository *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface) *Service {
-    return &Service{
-        UserService:  NewUserService(repository.UserRepository, bcrypt, jwtAuth),
-        AdminService: NewAdminService(repository.AdminRepository, repository.UserRepository, bcrypt),
-        FoodService:  NewFoodService(repository.FoodRepository, repository.AdminRepository),
-    }
+	return &Service{
+		UserService:  NewUserService(repository.UserRepository, bcrypt, jwtAuth),
+		AdminService: NewAdminService(repository.AdminRepository, repository.UserRepository, bcrypt),
+		FoodService:  NewFoodService(repository.FoodRepository, repository.AdminRepository),
+		OrderService: NewOrderService(repository.OrderRepository, repository.FoodRepository, repository.AdminRepository),
+	}
 }
